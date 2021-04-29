558a55fcafb93069a9a12fc436a9b8dde036a386
Palak Nath <palaknath@gmail.com>
2021-04-29 23:18:07 +0530

    db connection

---
+++ src/php/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+//database_connection.php
+/* $connect = new PDO("mysql:host=localhost;dbname=attendancedbFinal", "root", ""); */
+
+$con=mysqli_connect('localhost','root','');
+$db=mysqli_select_db($con,'WT_Project');
+
+if ($con) {
+    echo 'connected';
+  } else {
+    echo 'not connected';
+  }
+
+?>
