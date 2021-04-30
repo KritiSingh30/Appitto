ccc25249eceaedc0538907a75563934f604b32bf
Palak Nath <palaknath@gmail.com>
2021-04-30 11:43:00 +0530

    php

--- src/php/contact_us.php
+++ src/php/contact_us.php
@@ -2,15 +2,15 @@
 
 include("../php/dbconnect.php");
 
-$guests=$_POST['guests'];
-$phone=$_POST['phone'];
-$reservedate = $_POST['bookingtime'];
+$fname=$_POST['fullname'];
+$email_id=$_POST['email_id'];
+$msg = $_POST['message'];
 
 /* $guests=3;
 $phone=365673;
 $reservedate = '2021-04-30 00:21:09'; */
 
-$sql = "INSERT INTO reservation (num_guests,phone,reserve_date) VALUES ('$guests','$phone','$reservedate')";
+$sql = "INSERT INTO contact_us (fullname,email,message) VALUES ('$fname','$email_id','$msg')";
 
 if ($con->query($sql) === TRUE)
 { echo "New record created successfully";}
