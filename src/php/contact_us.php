79f10cc9182f1d118289573bd3b007e0cc7ab422
Palak Nath <palaknath@gmail.com>
2021-05-07 01:24:44 +0530

    Database Connection

--- src/php/contact_us.php
+++ src/php/contact_us.php
@@ -18,3 +18,25 @@ if ($con->query($sql) === TRUE)
   { echo "Error: " . $sql . "<br>" . $con->error;}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4.3.2/css/metro-all.min.css">
+    <script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://localhost/Appitto/src/css/contact.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cookie&family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <title>Contact Us</title>
+</head>
+    
+<body>
+<h1>Thank you for reaching out to us!</h1>
+<a href="../html/landing.php">Take me back to the Home Page</a>
+</body>
+</html>
